55ef9062da2f3c9bd7596ce538efdd6cd4707d8a
Pasi <hotpasita@hotmail.com>
2014-10-15 14:50:00 +0300

    full ada

---
+++ ada/.tmp_versions/ada.mod
@@ -0,0 +1,2 @@
+/home/pi/ldd3-read-only/misc-modules/ada/ada.ko
+/home/pi/ldd3-read-only/misc-modules/ada/ada.o
--- ada/ada.c
+++ ada/ada.c
@@ -1,4 +1,5 @@
-/* LCD Simulator module
+/* Adafruit 1110 LCD and button driver
+ * built on mcp23s08 GPIO driver.
  * (c) Lauri Pirttiaho, 2014
  */
 
@@ -11,21 +12,327 @@
 #include <linux/device.h>
 #include <linux/cdev.h>
 #include <linux/fs.h>
-#include <linux/spi/mcp23s08.h>
-#include <linux/i2c.h>
 #include <linux/gpio.h>
-#include <linux/delay.h> //sleep
+#include <linux/workqueue.h>
+#include <linux/i2c.h>
+#include <linux/spi/mcp23s08.h>
+#include <linux/wait.h>
+#include <linux/spinlock.h>
+#include <linux/delay.h>
 
 /************************************************************
  * Module config
  */
 
-#define LCD_BUFFER_LENGTH 80
-#define MODULE_NAME "lcd"
+#define MODULE_NAME "ada"
 
 /************************************************************
  * Module data
  */
+
+static int gpiobase = 240;
+
+module_param(gpiobase, int, 0444);
+
+/************************************************************
+ * GPIO
+ */
+
+#define IN(pin) gpio_request_one(gpiobase+(pin), GPIOF_IN, #pin)
+#define DIN(pin) gpio_direction_input(gpiobase+(pin))
+#define OUT(pin) gpio_request_one(gpiobase+(pin), GPIOF_OUT_INIT_HIGH, #pin)
+#define OUTL(pin) gpio_request_one(gpiobase+(pin), GPIOF_OUT_INIT_LOW, #pin)
+#define DOUT(pin) gpio_direction_output(gpiobase+(pin))
+#define FREE(pin) gpio_free(gpiobase+(pin))
+
+#define SET(pin, value) gpio_set_value_cansleep(gpiobase+(pin), (value))
+#define GET(pin) gpio_get_value_cansleep(gpiobase+(pin))
+
+/************************************************************
+ * I2C client and mcp23s08 driver
+ */
+
+static struct i2c_adapter* bus1;
+static struct i2c_client* cli32;
+
+static void ioexpander_init(void)
+{
+  // This should detect errors and return them!
+
+  struct mcp23s08_platform_data mcp23017_pfdata = {
+    .chip = {
+      [0] = {
+	.is_present = 1,
+	.pullups = 0x001F /* pins [4:0] */
+      }
+    },
+    .base = gpiobase
+  };
+
+  struct i2c_board_info mcp23017_info = {
+    .type = "mcp23017",
+    .addr = 32,
+    .platform_data = &mcp23017_pfdata
+  };
+
+  bus1 = i2c_get_adapter(1);
+  cli32 = i2c_new_device(bus1, &mcp23017_info); 
+}
+
+static void ioexpander_exit(void)
+{
+  if (cli32) {
+    i2c_unregister_device(cli32);
+  }
+  if (bus1) {
+    i2c_put_adapter(bus1);
+  }
+}
+
+/************************************************************
+ * Button control
+ */
+
+// Buttons (5 buttons of the Adafruit 1110)
+#define SELECT 0
+#define RIGHT  1
+#define DOWN   2
+#define UP     3
+#define LEFT   4
+
+static void buttons_init(void)
+{
+  IN(SELECT);
+  IN(RIGHT);
+  IN(DOWN);
+  IN(UP);
+  IN(LEFT);
+}
+
+static void buttons_exit(void)
+{
+  FREE(SELECT);
+  FREE(RIGHT);
+  FREE(DOWN);
+  FREE(UP);
+  FREE(LEFT);
+}
+
+/************************************************************
+ * Backlights
+ */
+
+// LEDs (3 back-light leds of the Adafruit 1110)
+#define RED   6
+#define GREEN 7
+#define BLUE  8
+
+static void bl_init(void)
+{
+  OUT(RED);
+  OUT(GREEN);
+  OUT(BLUE);
+}
+
+static void bl_exit(void)
+{
+  DIN(RED);
+  DIN(GREEN);
+  DIN(BLUE);
+
+  FREE(RED);
+  FREE(GREEN);
+  FREE(BLUE);
+}
+
+static void bl_color_set(int rgb)
+{
+  if ((rgb & 0x000000ff) > 0x0000007f) {
+    SET(BLUE, 0);
+  } else {
+    SET(BLUE, 1);
+  }
+  if ((rgb & 0x0000ff00) > 0x00007f00) {
+    SET(GREEN, 0);
+  } else {
+    SET(GREEN, 1);
+  }
+  if ((rgb & 0x00ff0000) > 0x007f0000) {
+    SET(RED, 0);
+  } else {
+    SET(RED, 1);
+  }
+}
+
+int bl_color = 0;
+
+static int bl_set(const char *val, const struct kernel_param *kp)
+{
+  int color;
+
+  int n_read = sscanf(val, "0x%x", &color);
+  if (n_read != 1) return -EINVAL;
+
+  bl_color = color;
+  bl_color_set(color);
+
+  return 0;
+}
+
+static int bl_get(char *val, const struct kernel_param *kp)
+{
+  return sprintf(val, "0x%08x", bl_color);
+}
+
+static struct kernel_param_ops bl_ops = {
+  .set = bl_set,
+  .get = bl_get
+};
+
+module_param_cb(backlight_color, &bl_ops, &bl_color, 0644);
+
+/************************************************************
+ * Button scanner
+ */
+
+static int buttons_before;
+
+static int button_events;
+static spinlock_t button_events_sl;
+static wait_queue_head_t but_readq;
+
+module_param(button_events, int, 0644);
+
+static void scan_buttons(void)
+{
+  int buttons_now = 0;
+
+  for (int i = 0; i < 5; ++i) {
+    buttons_now |= GET(i) << i;
+  }
+
+  int new_events = (buttons_before & ~buttons_now) & 0x1F;
+
+  spin_lock(&button_events_sl);
+  button_events |= new_events;
+  spin_unlock(&button_events_sl);
+
+  if (new_events) {
+    wake_up_interruptible(&but_readq);
+  }
+
+  buttons_before = buttons_now;
+}
+
+// Scanning work queue. Timer won't work since GPIO calls
+// may block which is not allowed in timer's interrupt
+// context!
+
+static struct workqueue_struct* scanner_q;
+static struct delayed_work scanner_w;
+
+// Scanning frequency in Hz
+#define SCAN_FRQ 50
+
+static void scanner_work(struct work_struct *work)
+{
+  scan_buttons();
+  PREPARE_DELAYED_WORK(&scanner_w, scanner_work);
+  queue_delayed_work(scanner_q, &scanner_w, HZ/SCAN_FRQ);
+}
+
+static void scanner_init(void)
+{
+  buttons_before = 0x1F;
+  button_events = 0;
+  spin_lock_init(&button_events_sl);
+
+  scanner_q = alloc_workqueue(MODULE_NAME "_q", WQ_UNBOUND, 1);
+  INIT_DELAYED_WORK(&scanner_w, scanner_work);
+  queue_delayed_work(scanner_q, &scanner_w, HZ/SCAN_FRQ);
+}
+
+static void scanner_exit(void)
+{
+  cancel_delayed_work_sync(&scanner_w);
+  flush_workqueue(scanner_q);
+  destroy_workqueue(scanner_q);
+}
+
+/************************************************************
+ * Button file ops
+ */
+
+static int but_open(struct inode *inode, struct file *filp)
+{
+  filp->private_data = 0; // Event read flag
+  return 0;
+}
+
+static ssize_t but_read(
+    struct file *filp, char __user *ubuff, size_t len, loff_t *offs)
+{
+  int ret = 0;
+  char buffer[32];
+
+  if (filp->private_data) {
+    filp->private_data = 0;
+    return 0;
+  }
+
+  ret = wait_event_interruptible_exclusive(but_readq, button_events);
+  
+  // Wake up from interrupts, try again
+  if (ret) return -ERESTARTSYS;
+
+  filp->private_data = (void*) 1;
+  len = 1;
+
+  // Return first event and clear it
+  spin_lock(&button_events_sl);
+  for (int i = 0; i < 5; ++i) {
+    if (button_events & (1<<i)) {
+      buffer[0] = '0'+i;
+      button_events &= ~(1<<i);
+      break;
+    }
+  }
+  spin_unlock(&button_events_sl);
+
+  int uncopied = copy_to_user(ubuff, buffer, len);
+
+  if (uncopied) {
+    ret = -EFAULT;
+  } else {
+    ret = len;
+  }
+
+  return ret;
+}
+
+static ssize_t but_write(struct file *filp, const char __user *ubuff, size_t len, loff_t *offs)
+{
+  return -EPERM;
+}
+
+static int but_release(struct inode *inode, struct file *filp)
+{
+  return 0;
+}
+
+static struct file_operations but_fileops = {
+  .owner = THIS_MODULE,
+  .open = but_open,
+  .read = but_read,
+  .write = but_write,
+  .release = but_release
+};
+
+/************************************************************
+ * LCD buffer
+ */
+
+#define LCD_BUFFER_LENGTH 80
 
 static char lcd_buffer[LCD_BUFFER_LENGTH] =
 "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ9876543210zyxwvuts";
@@ -116,194 +423,100 @@ static struct kernel_param_ops display_ops = {
 module_param_cb(lcd_size, &size_ops, &lcd_size, 0644);
 module_param_cb(display, &display_ops, lcd_buffer, 0644);
 
-
-/***************
- * CLD HW driver
-*/
-static int gpiobase = 240;
-
-module_param(gpiobase, int, 0444);
 /************************************************************
- * Button control
+ * HD44780U (KS0066U) driver
  */
-//see adafruit page for schema
-// Buttons (5 buttons of the Adafruit 1110)
-#define SELECT 0
-#define RIGHT  1
-#define DOWN   2
-#define UP     3
-#define LEFT   4
 
-// LEDs (3 back-light leds of the Adafruit 1110)
-#define BL_RED   6
-#define BL_GREEN 7
-#define BL_BLUE  8
-
-#define LCD_DB7 9
-#define LCD_DB6 10
-#define LCD_DB5 11
-#define LCD_DB4 12
-#define LCD_E   13
-#define LCD_RW 14
+// LCD pins
 #define LCD_RS 15
+#define LCD_RW 14
+#define LCD_E  13
+#define LCD_D4 12
+#define LCD_D5 11
+#define LCD_D6 10
+#define LCD_D7  9
 
-#define IN(pin) gpio_request_one(gpiobase+(pin), GPIOF_IN, #pin)
-#define DIN(pin) gpio_direction_input(gpiobase+(pin))
-#define OUTH(pin) gpio_request_one(gpiobase+(pin), GPIOF_OUT_INIT_HIGH, #pin)
-#define OUTL(pin) gpio_request_one(gpiobase+(pin), GPIOF_OUT_INIT_LOW, #pin)
-#define DOUT(pin) gpio_direction_output(gpiobase+(pin))
-#define FREE(pin) gpio_free(gpiobase+(pin))
-
-#define SET(pin, value) gpio_set_value_cansleep(gpiobase+(pin), (value))
-#define GET(pin) gpio_get_value_cansleep(gpiobase+(pin))
-
-static void lcd_write_buffer_to_hw(void);
-
-static void write_nybble(int n){
-//    SET(LCD_E,0);
-    
-    SET(LCD_DB7,(n>>3)&1);
-    SET(LCD_DB6,(n>>2)&1);
-    SET(LCD_DB5,(n>>1)&1);
-    SET(LCD_DB4,(n>>0)&1);
-    
-//    SET(LCD_RW,0);
-//    SET(LCD_RS,0);    
-    SET(LCD_E,1);
-//    sleep(1);  //not needed because GPIO is actually behind I2C
-    SET(LCD_E,0);
-}
-static void write_byte(int b){
-    write_nybble(b>>4); //hi
-    write_nybble(b>>0);  //low
-    
-}
-
-static void write_data( int b){
-    SET(LCD_RS,1);
-    write_byte(b);
-}
-static void write_command( int b){
-    SET(LCD_RS,0);
-    write_byte(b);
-}
-
-
-static void lcd_hw_init(void){
-    OUTH(BL_RED);
-    OUTH(BL_GREEN);
-    OUTH(BL_BLUE);
-    OUTL(LCD_DB7);
-    OUTL(LCD_DB6);
-    OUTL(LCD_DB5);
-    OUTL(LCD_DB4);
-    OUTL(LCD_E);
-    OUTL(LCD_RW); //this is always down
-    OUTL(LCD_RS); //set at write_data
-    
-    // reset HD44780.pdf p.45
-    //make sure adafruit is in 8-bit mode
-    write_nybble(3);
-    //min 4.1ms delay
-    mdelay(4);
-    write_nybble(3);
-    write_nybble(3);
-    //finally, make sure adafruit is in 4-bit mode
-    write_nybble(2);
-    
-    write_command(0x28); //function set 2 lines, 5x8 font
-    write_command(0x0c); //display on
-    write_command(0x06); //left to right
-    
-    SET(BL_RED,0);
-    SET(BL_BLUE,0);
-    SET(BL_GREEN,0);
-    int i;
-    write_command(1); //clear display
-//    for (i=0x30;i<0x50;i++){
-//        write_data(i);
-//    }
-    lcd_write_buffer_to_hw();
-}
-
-static const int row_start[]={0,64,20,84};
-static void lcd_write_line(int line)
+static void lcd_write_nybble(int n)
 {
-    int i=0;
-    
-    write_command(0x80+row_start[line]); //set ddram address
-    int row_offset=line*20;
-    if (lcd_size.lines==2){
-        row_offset=line*40;
-    }
-    
-    for (i=0;i<lcd_size.characters;i++){
-        write_data(lcd_buffer[row_offset+i]);
-    }
+  SET(LCD_D4, (n>>0) & 1);
+  SET(LCD_D5, (n>>1) & 1);
+  SET(LCD_D6, (n>>2) & 1);
+  SET(LCD_D7, (n>>3) & 1);
+  SET(LCD_E, 1);
+  SET(LCD_E, 0);
 }
 
-
-static void lcd_write_buffer_to_hw(void)
+static void lcd_write_byte(int b)
 {
-    int i;
-    for (i=0;i<lcd_size.lines;i++){
-        lcd_write_line(i);
-    }
+  lcd_write_nybble(b>>4);
+  lcd_write_nybble(b>>0);
 }
 
-static void lcd_hw_exit(void){
-    FREE(BL_RED);
-    FREE(BL_GREEN);
-    FREE(BL_BLUE);
-    FREE(LCD_DB7);
-    FREE(LCD_DB6);
-    FREE(LCD_DB5);
-    FREE(LCD_DB4);
-    FREE(LCD_E);
-    FREE(LCD_RW);
-    FREE(LCD_RS);
-    
-}
-/***************
- Adafruit init/exit code
-*/
-static struct i2c_adapter* bus1;
-static struct i2c_client* cli32;
-
-static int ada_init(void){
-  struct mcp23s08_platform_data mcp23017_pfdata = {
-    .chip = {
-      [0] = {
-	.is_present = 1,
-	.pullups = 0x001F /* pins [4:0] */
-      }
-    },
-    .base = gpiobase
-  };
-
-  struct i2c_board_info mcp23017_info = {
-    .type = "mcp23017",
-    .addr = 32,
-    .platform_data = &mcp23017_pfdata
-  };
-
-  bus1 = i2c_get_adapter(1);//always works
-  cli32 = i2c_new_device(bus1, &mcp23017_info); 
-
-  if(!cli32) return -ENXIO;
-  return 0;
-
+static void lcd_write_data(int b)
+{
+  SET(LCD_RS, 1);
+  lcd_write_byte(b);
 }
 
-static void ada_exit(void){
-  if (cli32) {
-    i2c_unregister_device(cli32);
+static void lcd_write_cmd(int b)
+{
+  SET(LCD_RS, 0);
+  lcd_write_byte(b);
+}
+
+static void lcd_init(void)
+{
+  OUTL(LCD_RS);
+  OUTL(LCD_RW);
+  OUTL(LCD_E);
+  OUTL(LCD_D4);
+  OUTL(LCD_D5);
+  OUTL(LCD_D6);
+  OUTL(LCD_D7);
+
+  lcd_write_nybble(3);
+  mdelay(4);
+  lcd_write_nybble(3);
+  // lcd writes taek longer than the required delays
+  lcd_write_nybble(3);
+  lcd_write_nybble(2);
+
+  lcd_write_cmd(0x28); // 2 lines 5x8 font
+  lcd_write_cmd(0x0C); // Display on
+  lcd_write_cmd(0x06); // Cursor moves right
+  lcd_write_cmd(0x01); // Clear
+}
+
+static void lcd_exit(void)
+{
+  FREE(LCD_D7);
+  FREE(LCD_D6);
+  FREE(LCD_D5);
+  FREE(LCD_D4);
+  FREE(LCD_E);
+  FREE(LCD_RW);
+  FREE(LCD_RS);
+}
+
+static const int line_starts[] = {0, 64, 20, 84};
+static void lcd_copy_line(int line)
+{
+  lcd_write_cmd(0x80 + line_starts[line]);
+  int line_offset = 20;
+  if (lcd_size.lines == 2) {
+    line_offset = 40;
   }
-  if (bus1) {
-    i2c_put_adapter(bus1);
+  for (int i = 0; i < lcd_size.characters; ++i) {
+    lcd_write_data(lcd_buffer[i + line * line_offset]);
   }
 }
 
+static void lcd_write_to_panel(void)
+{
+  for (int i = 0; i < lcd_size.lines; ++i) {
+    lcd_copy_line(i);
+  }
+}
 
 /************************************************************
  * Write stream parser
@@ -544,22 +757,22 @@ static void wsp_process(write_stream_parser_t *parser)
 }
 
 /************************************************************
- * Fileops
+ * LCD file ops
  */
 
 typedef enum {
   DO_READ,
   READ_DONE
-} read_state_e;
+} lcd_read_state_e;
 
 typedef struct {
   write_stream_parser_t parser;
-  read_state_e read_state;
-} file_state_t;
+  lcd_read_state_e read_state;
+} lcd_file_state_t;
 
-static int dev_open(struct inode *inode, struct file *filp)
+static int lcd_open(struct inode *inode, struct file *filp)
 {
-  file_state_t *fs = kmalloc(sizeof(file_state_t), GFP_KERNEL);
+  lcd_file_state_t *fs = kmalloc(sizeof(lcd_file_state_t), GFP_KERNEL);
   if (!fs) return -ENOMEM;
 
   filp->private_data = fs;
@@ -570,10 +783,10 @@ static int dev_open(struct inode *inode, struct file *filp)
   return 0;
 }
 
-static ssize_t dev_read(
+static ssize_t lcd_read(
     struct file *filp, char __user *ubuff, size_t len, loff_t *offs)
 {
-  file_state_t *fs = filp->private_data;
+  lcd_file_state_t *fs = filp->private_data;
   if (fs->read_state == READ_DONE) {
     fs->read_state = DO_READ;
     return 0;
@@ -605,9 +818,9 @@ static ssize_t dev_read(
   return ret;
 }
 
-static ssize_t dev_write(struct file *filp, const char __user *ubuff, size_t len, loff_t *offs)
+static ssize_t lcd_write(struct file *filp, const char __user *ubuff, size_t len, loff_t *offs)
 {
-  file_state_t *fs = filp->private_data;
+  lcd_file_state_t *fs = filp->private_data;
 
   char *buffer = kmalloc(PAGE_SIZE, GFP_KERNEL);
   if (!buffer) return -ENOMEM;
@@ -628,13 +841,14 @@ static ssize_t dev_write(struct file *filp, const char __user *ubuff, size_t len
 
   wsp_process_init(&fs->parser, buffer, len);
   wsp_process(&fs->parser);
+  lcd_write_to_panel();
 
   kfree(buffer);
-  lcd_write_buffer_to_hw();
+
   return ret;
 }
 
-static int dev_release(struct inode *inode, struct file *filp)
+static int lcd_release(struct inode *inode, struct file *filp)
 {
   if (filp->private_data) {
     kfree(filp->private_data);
@@ -642,29 +856,31 @@ static int dev_release(struct inode *inode, struct file *filp)
   return 0;
 }
 
-static struct file_operations fileops = {
+static struct file_operations lcd_fileops = {
   .owner = THIS_MODULE,
-  .open = dev_open,
-  .read = dev_read,
-  .write = dev_write,
-  .release = dev_release
+  .open = lcd_open,
+  .read = lcd_read,
+  .write = lcd_write,
+  .release = lcd_release
 };
-
 
 /************************************************************
  * Device data
  */
 
 static struct class *class;
-static dev_t devnum;
-struct cdev cdev;
-struct device *dev;
+static dev_t but_devnum;
+static dev_t lcd_devnum;
+struct cdev but_cdev;
+struct cdev lcd_cdev;
+struct device *but_dev;
+struct device *lcd_dev;
 
 /************************************************************
  * Init and exit
  */
 
-static int lcd_init(void)
+static int ada_init(void)
 {
   int err = 0;
 
@@ -672,38 +888,58 @@ static int lcd_init(void)
   class = class_create(THIS_MODULE, MODULE_NAME);
 
   // Allocate device number
-  err = alloc_chrdev_region(&devnum, 0, 1, MODULE_NAME);
+  err = alloc_chrdev_region(&lcd_devnum, 0, 2, MODULE_NAME);
   if (err) {
     goto devnum_fail;
   }
+  but_devnum = MKDEV(MAJOR(lcd_devnum), MINOR(lcd_devnum)+1);
 
   // Create cdev
-  cdev_init(&cdev, &fileops);
-  err = cdev_add(&cdev, devnum, 1);
+  cdev_init(&lcd_cdev, &lcd_fileops);
+  err = cdev_add(&lcd_cdev, lcd_devnum, 1);
   if (err) {
-    goto dev_add_fail;
+    goto lcd_dev_add_fail;
+  }
+  cdev_init(&but_cdev, &but_fileops);
+  err = cdev_add(&but_cdev, but_devnum, 1);
+  if (err) {
+    goto but_dev_add_fail;
   }
 
-  // Create device to /dev
-  dev = device_create(class, NULL, devnum, NULL, MODULE_NAME);
-  if (IS_ERR(dev)) {
-    err = PTR_ERR(dev);
-    goto dev_create_fail;
+  // Create devices to /dev
+  lcd_dev = device_create(class, NULL, lcd_devnum, NULL, MODULE_NAME "lcd");
+  if (IS_ERR(lcd_dev)) {
+    err = PTR_ERR(lcd_dev);
+    goto lcd_dev_create_fail;
   }
 
-  err=ada_init();
-  if (err)
-  {goto ada_init_fail;}
-  lcd_hw_init();
+  but_dev = device_create(class, NULL, but_devnum, NULL, MODULE_NAME "but");
+  if (IS_ERR(but_dev)) {
+    err = PTR_ERR(but_dev);
+    goto but_dev_create_fail;
+  }
+
   // All OK
-  return 0;
- ada_init_fail:
- 
-  dev_create_fail:
-  cdev_del(&cdev);
+  init_waitqueue_head(&but_readq);
+  ioexpander_init();
+  bl_init();
+  buttons_init();
+  lcd_init();
+  scanner_init();
 
- dev_add_fail:
-  unregister_chrdev_region(devnum, 1);  
+  return 0;
+
+ but_dev_create_fail:
+  device_destroy(class, lcd_devnum);
+
+ lcd_dev_create_fail:
+  cdev_del(&but_cdev);
+
+ but_dev_add_fail:
+  cdev_del(&lcd_cdev);
+
+ lcd_dev_add_fail:
+  unregister_chrdev_region(lcd_devnum, 2);  
 
  devnum_fail:
   class_destroy(class);
@@ -711,18 +947,26 @@ static int lcd_init(void)
   return err;
 }
 
-static void lcd_exit(void)
+static void ada_exit(void)
 {
-    lcd_hw_exit();
-    ada_exit();
-  device_destroy(class, devnum);
-  cdev_del(&cdev);
-  unregister_chrdev_region(devnum, 1);  
+  printk(KERN_ALERT "---exit\n");  
+  
+  scanner_exit();
+  lcd_exit();
+  buttons_exit();
+  bl_exit();
+  ioexpander_exit();
+
+  device_destroy(class, but_devnum);
+  device_destroy(class, lcd_devnum);
+  cdev_del(&but_cdev);
+  cdev_del(&lcd_cdev); 
+  unregister_chrdev_region(lcd_devnum, 2);  
   class_destroy(class);
 }
 
-module_init(lcd_init);
-module_exit(lcd_exit);
+module_init(ada_init);
+module_exit(ada_exit);
 
 MODULE_AUTHOR("Lauri Pirttiaho <lapi@cw.fi>");
 MODULE_LICENSE("Dual BSD/GPL");
